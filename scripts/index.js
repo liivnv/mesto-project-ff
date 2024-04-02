b02c683b02c2161b3dc72c756443207650a0f101
liivnv <liivnvv@yandex.ru>
2024-04-02 12:25:11 +0300

    правки ревью-1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,8 @@
 // @todo: Темплейт карточки
 // @todo: DOM узлы
 // @todo: Функция создания карточки
-function createCards(initialCards, deleteCards){
+function createCard(initialCards, deleteCard){
   const cardTemplate = document.querySelector('#card-template').content;
-  const placesList = document.querySelector('.places__list');
 
   const userElement = cardTemplate.querySelector('.card').cloneNode(true);
   const deleteButton = userElement.querySelector('.card__delete-button');
@@ -11,17 +10,18 @@ function createCards(initialCards, deleteCards){
   userElement.querySelector('.card__title').textContent = initialCards.name;
   userElement.querySelector('.card__image').src = initialCards.link;
   userElement.querySelector('.card__image').alt = initialCards.alt;
-  
-  placesList.append(userElement);
 
-  deleteButton.addEventListener('click', deleteCards); 
+  deleteButton.addEventListener('click', deleteCard); 
+
+  return userElement;
 }
 // @todo: Функция удаления карточки
-function deleteCards(e){
-    const placesItem = e.target.closest('.places__item');
+function deleteCard(evt){
+    const placesItem = evt.target.closest('.places__item');
     placesItem.remove();
 }
 // @todo: Вывести карточки на страницу
+const placesList = document.querySelector('.places__list');
 for (let i=0; i<initialCards.length; i++){
-  createCards(initialCards[i], deleteCards);
+  placesList.append(createCard(initialCards[i], deleteCard));
 }

d387f97f5db6bfada25955d6dbfccec68472790c
liivnv <liivnvv@yandex.ru>
2024-04-02 11:48:30 +0300

    ревью-1

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/liivnv/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
 // @todo: Темплейт карточки
-
 // @todo: DOM узлы
-
 // @todo: Функция создания карточки
+function createCards(initialCards, deleteCards){
+  const cardTemplate = document.querySelector('#card-template').content;
+  const placesList = document.querySelector('.places__list');
 
+  const userElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = userElement.querySelector('.card__delete-button');
+
+  userElement.querySelector('.card__title').textContent = initialCards.name;
+  userElement.querySelector('.card__image').src = initialCards.link;
+  userElement.querySelector('.card__image').alt = initialCards.alt;
+  
+  placesList.append(userElement);
+
+  deleteButton.addEventListener('click', deleteCards); 
+}
 // @todo: Функция удаления карточки
-
+function deleteCards(e){
+    const placesItem = e.target.closest('.places__item');
+    placesItem.remove();
+}
 // @todo: Вывести карточки на страницу
+for (let i=0; i<initialCards.length; i++){
+  createCards(initialCards[i], deleteCards);
+}
